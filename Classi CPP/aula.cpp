30d4c1a49aee771b8c9604f00ba5ec1438f9e240
Andropiro <andrea.pironti99@yahoo.it>
2017-05-30 22:21:37 +0200

    Add files via upload

---
+++ Classi CPP/aula.cpp
@@ -0,0 +1,20 @@
+/*
+Codice scritto da: Andrea Pironti
+*/
+
+#include <iostream>
+#include "aula.h"
+
+//Setters
+
+void Aula::setNumero(int numero)
+{
+	numero = numero;
+}
+
+//Getters
+
+char Aula::getNumero()
+{
+	return numero;
+}
---
+++ Classi CPP/materia.cpp
@@ -0,0 +1,20 @@
+/*
+Codice scritto da: Andrea Pironti
+*/
+
+#include <iostream>
+#include "materia.h"
+
+//Setters
+
+void Materia::setNome(char nome)
+{
+	nome = nome;
+}
+
+//Getters
+
+char Materia::getNome()
+{
+	return nome;
+}
