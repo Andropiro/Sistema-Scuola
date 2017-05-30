02f6045d71f50233a7309292461ee9e3ef94309c
Andropiro <andrea.pironti99@yahoo.it>
2017-05-30 22:22:34 +0200

    Add files via upload

---
+++ Classi H/aula.h
@@ -0,0 +1,12 @@
+/*
+Codice scritto da: Andrea Pironti
+*/
+
+#pragma once
+class Aula
+{
+public:
+	int numero;	//Numero dell'aula
+	void setNumero(int numero);
+	char getNumero();
+};
---
+++ Classi H/materia.h
@@ -0,0 +1,12 @@
+/*
+Codice scritto da: Andrea Pironti
+*/
+
+#pragma once
+class Materia
+{
+public:
+	char nome;	//Nome della materia
+	void setNome(char nome);
+	char getNome();
+};
